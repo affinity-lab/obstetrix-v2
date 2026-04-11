1156a26716bdd3e12e7526ba36a603ea1699359e
Aranykacsa <zalanvadasz@gmail.com>
2026-04-11 09:13:51 +0200

    mod: fixing installer

--- packages/gui/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from 'tailwindcss';
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: { extend: {} },
-  plugins: [require('@atom-forge/ui/tailwind')],
-} satisfies Config;
